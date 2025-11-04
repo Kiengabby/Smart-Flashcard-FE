e231ec9182a0505d4470b2670cb1863d4939626a
kienhm <kienhm.evnit@evn.com.vn>
2025-11-04 17:08:22 +0700

    Remove terms of service checkbox from registration form
    - Remove agreeToTerms field from registration form
    - Remove terms of service checkbox from HTML template
    - Remove NzCheckboxModule import and related CSS
    - Simplify registration validation to only require basic fields

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -8,7 +8,6 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { AuthService } from '../../services/auth.service';
 
@@ -35,8 +34,7 @@ export function passwordMatchValidator(control: AbstractControl): ValidationErro
     NzInputModule,
     NzButtonModule,
     NzIconModule,
-    NzDividerModule,
-    NzCheckboxModule
+    NzDividerModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
@@ -67,8 +65,7 @@ export class RegisterComponent implements OnInit {
       displayName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]],
-      agreeToTerms: [false, [Validators.requiredTrue]]
+      confirmPassword: ['', [Validators.required]]
     }, {
       validators: passwordMatchValidator // Custom validator cho toàn bộ form
     });
